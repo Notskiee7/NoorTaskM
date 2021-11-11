1a1d39f366d0edc4f8eaf49ccac2e521bef27b56
karawan <banat811@gmail.com>
2021-11-11 08:55:36 +0200

    after designing all activities all activities' graphics components (view) declared and found view by id

--- app/src/main/java/noor/application/noortaskm/SplashScreenActivity.java
+++ app/src/main/java/noor/application/noortaskm/SplashScreenActivity.java
@@ -2,6 +2,9 @@ package noor.application.noortaskm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -13,5 +16,25 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         IVSplash=findViewById(R.id.IVSplash);
+// thread:1
+        Thread th=new Thread(){
+            //thred:2
+            @Override
+            public void run() {
+                //هنا المقطع الذي سيعمل بالتزامن مع المقاطع
+                //thread:3
+                int ms=3*1000;
+                try {
+                    sleep(ms);
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        //thread:4
+        th.start();
+
     }
 }
